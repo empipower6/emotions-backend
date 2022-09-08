ff1eae2ddd530ff8338b53b5c7f055de54e0fb1d
Emre Kelleci <35880247+empipower6@users.noreply.github.com>
2022-09-07 22:36:58 -0400

    new words post request sent

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 
+import re
 from flask import Flask,request
 from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
+import csv
+import json
 
 from nrclex import NRCLex
 import nltk
 import ssl
+
 
 
 
@@ -22,6 +26,58 @@ nltk.download('punkt')
 
 CORS(app)
 
+#Download gunter's words
+# Opening JSON file
+f = open('emotionChart/words.json')
+ # returns JSON object as 
+ # a dictionary
+data = json.load(f)
+f.close()
+
+#Functions 
+def analyzeWord(word):
+  for char in data:
+   if char["word"] == word:
+    return char
+  return False
+
+def analyzeText(txt):
+        noCharText= re.sub(r'[^\w]', ' ', txt)
+        wordArr = noCharText.split(' ')
+        emotionWordCount = 0
+        emotionSum = {
+        "POSITIVE": 0,
+        "NEGATIVE": 0,
+        "ANGER": 0,
+        "ANTICIPATION": 0,
+        "DISGUST": 0,
+        "FEAR": 0,
+        "JOY": 0,
+        "SADNESS": 0,
+        "SURPRISE": 0,
+        "TRUST": 0
+        }
+        for word in wordArr:
+                wordEmotions = analyzeWord(word)
+                if wordEmotions:
+                        emotionSum['POSITIVE'] += wordEmotions['POSITIVE']
+                        emotionSum['NEGATIVE'] += wordEmotions['NEGATIVE']
+                        emotionSum['ANGER'] += wordEmotions['ANGER']
+                        emotionSum['ANTICIPATION'] += wordEmotions['ANTICIPATION']
+                        emotionSum['DISGUST'] += wordEmotions['DISGUST']
+                        emotionSum['FEAR'] += wordEmotions['FEAR']
+                        emotionSum['JOY'] += wordEmotions['JOY']
+                        emotionSum['SADNESS'] += wordEmotions['SADNESS']
+                        emotionSum['SURPRISE'] += wordEmotions['SURPRISE']
+                        emotionSum['TRUST'] += wordEmotions['TRUST']
+                        emotionWordCount += 1
+        
+        for emotion in emotionSum:
+                emotionSum[emotion] = round((emotionSum[emotion]*100)/emotionWordCount,1)
+        return emotionSum
+
+
+#Routes
 @app.route('/api/analyze',methods=['POST'])
 @cross_origin()
 def analysis ():
@@ -45,10 +101,14 @@ def analysis ():
     else:
             return {'results':"Something went wrong"}
 
-@app.route('/api/welcome')
-@cross_origin()
-def Welcome():
-    return "Welcome to the API!!!"
+
+@app.route('/api/gunterwords',methods=['POST'])
+def gunterwords():
+ req = request.get_json()
+
+ result = analyzeText(req['textAnalyze'])
+ return result
+         
 
 @app.route('/')
 @cross_origin()
